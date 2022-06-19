ad751ef7a3257fc3dfc5554962dfd80fa1de3488
Logtism <78323275+Logtism@users.noreply.github.com>
2022-06-20 10:45:36 +1200

    Added ramdom item to the home page
    - Added random items to home page

--- base/views.py
+++ base/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from items.models import Item
+from random import choice
 
 
 def home(request):
-    return render(request, 'base/home.html')
+    all_items = Item.objects.all()
+    
+    pks = Item.objects.filter(accepted=True).values_list('pk', flat=True)
+    
+    items = []
+    
+    while len(items) < 6:
+        random_pk = choice(pks)
+        item = Item.objects.get(id=random_pk)
+        if item not in items:
+            items.append(item)
+    
+    return render(request, 'base/home.html', {'items': items})
