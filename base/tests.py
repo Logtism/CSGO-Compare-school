c1e7cb4933b757eddd7027523879f5e6ff70cf0a
Logtism <78323275+Logtism@users.noreply.github.com>
2022-06-21 09:14:43 +1200

    Fixed getting prices
    - Made JavaScript to get from the api on the pricing info
    - Fixed home page so it will not error if no items in the database
    - Fixed home pages tests

--- base/tests.py
+++ base/tests.py
@@ -1,10 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.core.management import call_command
 from firebrick.tests import ResolveUrlTest, GetViewTest
 from . import views
 
 
-class TestHome(TestCase, ResolveUrlTest, GetViewTest):
+class TestHome(TestCase, ResolveUrlTest):
     name = 'base-home'
     view = views.home
     template = 'base/home.html'
-    status = 200
+    
+    def test_get(self):
+        call_command('loaddata', 'catogory', 'subcategory', 'rarity', 'collection', 'container', 'item', verbosity=0)
+        
+        client = Client()
+
+        response = client.get(reverse(self.name))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, self.template)
--- base/views.py
+++ base/views.py
@@ -9,11 +9,11 @@ def home(request):
     pks = Item.objects.filter(accepted=True).values_list('pk', flat=True)
     
     items = []
-    
-    while len(items) < 6:
-        random_pk = choice(pks)
-        item = Item.objects.get(id=random_pk)
-        if item not in items:
-            items.append(item)
+    if len(pks) > 0:
+        while len(items) < 6:
+            random_pk = choice(pks)
+            item = Item.objects.get(id=random_pk)
+            if item not in items:
+                items.append(item)
     
     return render(request, 'base/home.html', {'items': items})
